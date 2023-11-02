1b2e14dee604e9bdf4d7f17612a66a63bc3a134a
Lucas Elsen <l.elsen@tu-braunschweig.de>
2023-11-02 23:48:05 +0100

    Add a simple HTTP answer
    For now requests are not parsed at all. Everytime 200 OK is returned and the connection is closed.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
+fn handle_request(_request: &str) -> &str {
+    "HTTP/1.1 200 OK\r\n\r\n"
+}
+
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:5555")?;
 
     while let Some(Ok(mut stream)) = listener.incoming().next() {
-        stream.write(b"Welcome to my echo service. Please write a message:\n")?;
         let mut buffer = [0; 1024];
         stream.read(&mut buffer)?;
 
+        // TODO: Here the request should be parsed in the future
         let request = std::str::from_utf8(&buffer).unwrap();
+        println!("------ Request\n{request}\n------ End Request");
 
-        println!("Got Answer: {request}");
+        let response = handle_request(request);
+        println!("------ Response\n{response}\n------ End Response");
 
-        stream.write(format!("Echo: {}", request).as_bytes())?;
+        stream.write(response.as_bytes())?;
     }
 
     Ok(())
