89c868264fd68a629ffc4e58e1fb52e8b6a172cb
Lucas Elsen <l.elsen@tu-braunschweig.de>
2023-11-02 23:17:45 +0100

    Implement a simple echo server
    Write a simple echo server that echos the request back to the client and then closes the connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:5555")?;
+
+    while let Some(Ok(mut stream)) = listener.incoming().next() {
+        stream.write(b"Welcome to my echo service. Please write a message:\n")?;
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer)?;
+
+        let request = std::str::from_utf8(&buffer).unwrap();
+
+        println!("Got Answer: {request}");
+
+        stream.write(format!("Echo: {}", request).as_bytes())?;
+    }
+
+    Ok(())
 }
